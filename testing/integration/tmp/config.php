5e3edeaedcf37aeeb4c807410af9726956771d09
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-01-07 19:16:41 -0500

    Initial Commit

---
+++ testing/integration/tmp/config.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'debug' => true,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'port' => 3306,
+    'database' => 'flarum_test',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => false,
+    'engine' => 'InnoDB',
+    'prefix_indexes' => true,
+  ),
+  'url' => 'http://localhost',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
